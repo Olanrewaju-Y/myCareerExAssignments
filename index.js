4e69a38d47ba5e904149fd2fb5d4eeb1e07865f4
Lyrix kyrix <lyrix7000@gmail.com>
2025-04-15 20:19:48 +0100

    added Index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+// const name = "James";
+// const age = 20;
+// const isMarried = true;
+
+// Arrays
+
+// const names = ["james", "kola", "john"]
+// console.log(names);
+// console.log(names[1]);
+
+// Objects
+
+// const families = {
+//     father: "David",
+//     mother: "Mrs David",
+//     sons: ["seyi", "bayo", "Tolu"],
+//     businesses: {
+//         agriculture: "agroBiz",
+//         banking: "MoniePoint Bank",
+//         engineering: "247Elect"      
+//         },
+//     housesCount: 20,
+// }
+// console.log(families);
+// console.log(families.sons[2]);
+// console.log(families.businesses.banking);
+// console.log(families.housesCount);
+
+// Functions
+
+// function sayHey() {
+//     console.log("Say Hey!");
+// }
+
+// sayHey();
+
+let sayHy = () => {
+    console.log("Say Hy!".toLocaleUpperCase().);
+}
+sayHy();
+
+// objects and more
+// right now got it i guess or not
+
+let test = 01
