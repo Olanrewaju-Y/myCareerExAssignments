8b536aff6a84079da946078e4ce67a81e9359b1e
Olanrewaju <lyrix7000@gmail.com>
2025-04-21 18:15:29 +0100

    onlyAssignment10Remains

--- index.js
+++ index.js
@@ -1,263 +1,263 @@
-// WEEK THREE ASSIGNMENTS
+// // WEEK THREE ASSIGNMENTS
 
-// CONCENCATE
-const name = "Olanrewaju";
-const age = 35;
-const nationality = "Nigeria";
+// // CONCENCATE
+// const name = "Olanrewaju";
+// const age = 35;
+// const nationality = "Nigeria";
 
-console.log(
-  "My name is " +
-    name +
-    ", i'm a" +
-    " " +
-    age +
-    "years old from " +
-    nationality +
-    "."
-);
+// console.log(
+//   "My name is " +
+//     name +
+//     ", i'm a" +
+//     " " +
+//     age +
+//     "years old from " +
+//     nationality +
+//     "."
+// );
 
-// UPPERCASING AND LOWERCASING
+// // UPPERCASING AND LOWERCASING
 
-const favQoute = "Time is Money";
+// const favQoute = "Time is Money";
 
-console.log(favQoute.toUpperCase());
-console.log(favQoute.toLowerCase());
+// console.log(favQoute.toUpperCase());
+// console.log(favQoute.toLowerCase());
 
-// REVERSE A WORD
+// // REVERSE A WORD
 
-const sex = "female";
-console.log(sex.split("").reverse().join(""));
+// const sex = "female";
+// console.log(sex.split("").reverse().join(""));
 
-// CAL. TOTAL AMOUNT AND DISPLAY
+// // CAL. TOTAL AMOUNT AND DISPLAY
 
-const garriPrice = 100;
-const water = 50;
-const sugar = 50;
+// const garriPrice = 100;
+// const water = 50;
+// const sugar = 50;
 
-console.log("The G.Total = " + (garriPrice + water + sugar) + "naira");
+// console.log("The G.Total = " + (garriPrice + water + sugar) + "naira");
 
-//5.  TEST SCORES AVERAGE
+// //5.  TEST SCORES AVERAGE
 
-const studentName = "James";
-const eng = 40;
-const math = 75;
-const biology = 55;
-const chemistry = 80;
-const geography = 85;
+// const studentName = "James";
+// const eng = 40;
+// const math = 75;
+// const biology = 55;
+// const chemistry = 80;
+// const geography = 85;
 
-console.log(
-  "The average score of " +
-    studentName +
-    " an ss3 Student is " +
-    (eng + math + biology + chemistry + geography) +
-    "points"
-);
+// console.log(
+//   "The average score of " +
+//     studentName +
+//     " an ss3 Student is " +
+//     (eng + math + biology + chemistry + geography) +
+//     "points"
+// );
 
-//6.  ARRAY
+// //6.  ARRAY
 
-let favFood = ["Bread", "dodo", "Rice", "Yam", "Beans"];
+// let favFood = ["Bread", "dodo", "Rice", "Yam", "Beans"];
 
-console.log(favFood[0] + " and " + favFood[4] + " is my best food combination");
+// console.log(favFood[0] + " and " + favFood[4] + " is my best food combination");
 
-//7.  ADD ITEMS TO FIRST AND LAST
+// //7.  ADD ITEMS TO FIRST AND LAST
 
-// favFood.unshift("amala")
-favFood = ["amala", ...favFood];
+// // favFood.unshift("amala")
+// favFood = ["amala", ...favFood];
 
-// favFood.push("Mango")
-favFood = [...favFood, "Mango"];
+// // favFood.push("Mango")
+// favFood = [...favFood, "Mango"];
 
-console.log(favFood);
-// console.log(favFood.length);
+// console.log(favFood);
+// // console.log(favFood.length);
 
-//8. jssOne, jssTwo, jssThree ARRAY
+// //8. jssOne, jssTwo, jssThree ARRAY
 
-const jssOne = [
-  "james",
-  "kola",
-  "john",
-  "Rashida",
-  "Kudi",
-  "bukki",
-  "Debby",
-  "Tope",
-  "Ope",
-  "Ugo",
-];
+// const jssOne = [
+//   "james",
+//   "kola",
+//   "john",
+//   "Rashida",
+//   "Kudi",
+//   "bukki",
+//   "Debby",
+//   "Tope",
+//   "Ope",
+//   "Ugo",
+// ];
 
-const jssTwo = [
-  "james",
-  "kola",
-  "john",
-  "Rashida",
-  "Kudi",
-  "bukki",
-  "Debby",
-  "Tope",
-  "Ope",
-  "Ugo",
-];
+// const jssTwo = [
+//   "james",
+//   "kola",
+//   "john",
+//   "Rashida",
+//   "Kudi",
+//   "bukki",
+//   "Debby",
+//   "Tope",
+//   "Ope",
+//   "Ugo",
+// ];
 
-const jssThree = [
-  "james",
-  "kola",
-  "john",
-  "Rashida",
-  "Kudi",
-  "bukki",
-  "Debby",
-  "Tope",
-  "Ope",
-  "Ugo",
-];
+// const jssThree = [
+//   "james",
+//   "kola",
+//   "john",
+//   "Rashida",
+//   "Kudi",
+//   "bukki",
+//   "Debby",
+//   "Tope",
+//   "Ope",
+//   "Ugo",
+// ];
 
-console.log(jssOne);
-console.log(jssTwo);
-console.log(jssThree);
-
-
-
-// WEEK 4 ASSIGNMENTS
+// console.log(jssOne);
+// console.log(jssTwo);
+// console.log(jssThree);
 
 
-//1. FUNCTION score >= 180
 
-function scoreResult(score) {
-  if (score >= 180) {
-    console.log("Passed!");
-  } else {
-    console.log("Failed!");
-  }
-}
+// // WEEK 4 ASSIGNMENTS
 
-scoreResult(190);
 
-//2.  FUNCTION age
+// //1. FUNCTION score >= 180
 
-function ageResult(age) {
-  if (age >= 18) {
-    console.log("You are Eligible to Vote!");
-  } else {
-    console.log("NOT Eligible!");
-  }
-}
-
-ageResult(17);
-
-//2.  FUNCTION name, score
-
-function nameNScore(name, score) {
-  if (score >= 90) {
-    console.log(name + ": " + "Excellent!");
-  } else if (score >= 75) {
-    console.log(name + ": " + "Good");
-  } else if (score >= 50) {
-    console.log(name + ": " + "Average");
-  } else {
-    console.log(name + ": " + "Fail!");
-  }
-}
-
-nameNScore("Tolu", 0);
-
-//4. BOOLEAN FUNCTION
-
-function booleanFunction(hasID, isAbove18) {
-  if (hasID && isAbove18 === true) {
-    console.log("Access granted!");
-  } else {
-    console.log("Access Denied!");
-  }
-}
-
-booleanFunction(true, true);
-
-//5. FOR LOOP
-
-const studentScore = [10, 51, 90, 59, 75, 40, 60, 100];
-
-for (let a = 0; a < studentScore.length; a++) {
-  //  console.log(studentScore[a]);
-  //  const sScore = studentScore[a];
-  if (studentScore[a] > 50) {
-    console.log("Passed!");
-  } else {
-    console.log("Fail!");
-  }
-}
-
-//6. ARROW FUNCTION
-// const math001 = 50
-// const eng001 = 10
-
-const passed = (math001, eng001) => {
-  if (math001 && eng001 >= 50) {
-    console.log("Yes");
-  } else {
-    console.log("No");
-  }
-};
-passed(90, 50);
-
-//7. FUNCTION CHECK EMAIL OR PHONE IS AVAILABLE
-// const email = true
-// const phone = false
-
-function emailNPhone(email, phone) {
-  if (email === true || phone === true) {
-    console.log("Allow to SignUp!");
-  } else {
-    console.log("Deny SignUp!");
-  }
-}
-emailNPhone(false, false);
-
-//8. FUNCTION USERNAME OR PASSWORD IS EMPTY
-
-function usernameOrPassword(usrname, passwrd) {
-  // if(usrname || passwrd === "" )
-
-  if (usrname === "" || passwrd === "") {
-    console.log("Invalid Input");
-  } else {
-    console.log("Valid Input");
-  }
-}
-usernameOrPassword("65165465", "");
-
-//9. USING TERNARY OPERATOR
-
-// function hourChecker(hours) {
-//   return hours >= 40 ? "Full-time" : "Part-time";
+// function scoreResult(score) {
+//   if (score >= 180) {
+//     console.log("Passed!");
+//   } else {
+//     console.log("Failed!");
+//   }
 // }
-  const hours = 41;
-  const hourChecker = hours >= 40 ? "Full-time" : "Part-time" ;
-    console.log(hourChecker);
 
-//10. ARROW FUNCTION RETURN LARGER ONE
+// scoreResult(190);
 
-const toLargerNumber = (x, y) => x > y ? x : y;
+// //2.  FUNCTION age
 
-console.log(toLargerNumber(105, 100));
+// function ageResult(age) {
+//   if (age >= 18) {
+//     console.log("You are Eligible to Vote!");
+//   } else {
+//     console.log("NOT Eligible!");
+//   }
+// }
+
+// ageResult(17);
+
+// //2.  FUNCTION name, score
+
+// function nameNScore(name, score) {
+//   if (score >= 90) {
+//     console.log(name + ": " + "Excellent!");
+//   } else if (score >= 75) {
+//     console.log(name + ": " + "Good");
+//   } else if (score >= 50) {
+//     console.log(name + ": " + "Average");
+//   } else {
+//     console.log(name + ": " + "Fail!");
+//   }
+// }
+
+// nameNScore("Tolu", 0);
+
+// //4. BOOLEAN FUNCTION
+
+// function booleanFunction(hasID, isAbove18) {
+//   if (hasID && isAbove18 === true) {
+//     console.log("Access granted!");
+//   } else {
+//     console.log("Access Denied!");
+//   }
+// }
+
+// booleanFunction(true, true);
+
+// //5. FOR LOOP
+
+// const studentScore = [10, 51, 90, 59, 75, 40, 60, 100];
+
+// for (let a = 0; a < studentScore.length; a++) {
+//   //  console.log(studentScore[a]);
+//   //  const sScore = studentScore[a];
+//   if (studentScore[a] > 50) {
+//     console.log("Passed!");
+//   } else {
+//     console.log("Fail!");
+//   }
+// }
+
+// //6. ARROW FUNCTION
+// // const math001 = 50
+// // const eng001 = 10
+
+// const passed = (math001, eng001) => {
+//   if (math001 && eng001 >= 50) {
+//     console.log("Yes");
+//   } else {
+//     console.log("No");
+//   }
+// };
+// passed(90, 50);
+
+// //7. FUNCTION CHECK EMAIL OR PHONE IS AVAILABLE
+// // const email = true
+// // const phone = false
+
+// function emailNPhone(email, phone) {
+//   if (email === true || phone === true) {
+//     console.log("Allow to SignUp!");
+//   } else {
+//     console.log("Deny SignUp!");
+//   }
+// }
+// emailNPhone(false, false);
+
+// //8. FUNCTION USERNAME OR PASSWORD IS EMPTY
+
+// function usernameOrPassword(usrname, passwrd) {
+//   // if(usrname || passwrd === "" )
+
+//   if (usrname === "" || passwrd === "") {
+//     console.log("Invalid Input");
+//   } else {
+//     console.log("Valid Input");
+//   }
+// }
+// usernameOrPassword("65165465", "");
+
+// //9. USING TERNARY OPERATOR
+
+// // function hourChecker(hours) {
+// //   return hours >= 40 ? "Full-time" : "Part-time";
+// // }
+//   const hours = 41;
+//   const hourChecker = hours >= 40 ? "Full-time" : "Part-time" ;
+//     console.log(hourChecker);
+
+// //10. ARROW FUNCTION RETURN LARGER ONE
+
+// const toLargerNumber = (x, y) => x > y ? x : y;
+
+// console.log(toLargerNumber(105, 100));
 
 
-// DESIGNING A SEARCH 
+// // DESIGNING A SEARCH 
 
-const dataBase = [
-  "james",  "kola",  "john",  "Rashida",  "Kudi",  "bukki",  "Debby",  "Tope",  "Ope",  "Ugo", "200" , "300"
-];
+// const dataBase = [
+//   "james",  "kola",  "john",  "Rashida",  "Kudi",  "bukki",  "Debby",  "Tope",  "Ope",  "Ugo", "200" , "300"
+// ];
 
-function inputchecker(searcherInput) {
-  if (dataBase.map(name => name.toLowerCase()).includes(searcherInput.toLowerCase())) {
-    console.log(searcherInput);
-} else {
-  console.log("Not avaliable!");
-}
-}
+// function inputchecker(searcherInput) {
+//   if (dataBase.map(name => name.toLowerCase()).includes(searcherInput.toLowerCase())) {
+//     console.log(searcherInput);
+// } else {
+//   console.log("Not avaliable!");
+// }
+// }
 
-inputchecker("200")
+// inputchecker("200")
  
-console.log(toLargerNumber(100, 220));
+// console.log(toLargerNumber(100, 220));
 
 
 // WEEK 5 ASSIGNMENT
@@ -290,25 +290,64 @@ const drugs = [
 
 // 1. Get all drugs that are antibiotics.
 
-const getAntibiotic = drugs.filter(drug => drug.category === "Antibiotic")
-console.log(getAntibiotic);
+// const getAntibiotic = drugs.filter(drug => drug.category === "Antibiotic")
+// console.log(getAntibiotic);
 
-// 2. Return an array of drug names in lowercase.
+// // 2. Return an array of drug names in lowercase.
 
-// 3. Write a function that accepts a category and returns all drugs under that category.
+//   const drugNameInLowerCase = drugs.map(drug => drug.name.toLocaleLowerCase());
+//   console.log(drugNameInLowerCase);
 
-// 4. Log each drug’s name and its manufacturer.
 
-// 5. Return all drugs that require a prescription.
+// // 3. Write a function that accepts a category and returns all drugs under that category.
 
-// 6. Return a new array, each item should follow the format: "Drug: [name] - [dosageMg]mg".
+// function rtnDrugsInCategory(cat) {
 
-// 7. Write a function that returns all drugs with a stock less than 50.
+//  return drugsInCategory = drugs.filter(drug => drug.category.toLocaleLowerCase() === cat.toLocaleLowerCase());
+//   //  return drugsInCategory;
+// }
+// console.log(rtnDrugsInCategory("aNtIbiotIC"));
 
-// 8. Return all drugs that are not prescription-only.
+// // 4. Log each drug’s name and its manufacturer.
+   
+//     const drugName = drugs.map(drug => "name: " + drug.name + ", " + "manufacturer: " + drug.manufacturer)
 
-// 9. Write a function that takes a manufacturer name and returns how many drugs are from that company.
+//     console.log(drugName);
+
+// // 5. Return all drugs that require a prescription.
+//    const prescriptionDrugs = drugs.filter( drug => drug.isPrescriptionOnly === true)
+//    console.log(prescriptionDrugs);
+
+// // 6. Return a new array, each item should follow the format: "Drug: [name] - [dosageMg]mg".
+
+//    const nameNDosageMg = drugs.map(drug => "Drug: " + drug.name + " - " + drug.dosageMg + "mg");
+//    console.log(nameNDosageMg);
+
+// // 7. Write a function that returns all drugs with a stock less than 50.
+
+//   function less50Drugs() {
+//    return drugStock = drugs.filter(drug => drug.stock < 50)
+//     // return drugStock;
+//   }
+//   console.log(less50Drugs());
+
+// // 8. Return all drugs that are not prescription-only.
+
+//    const notPrescription = drugs.filter(drug => drug.isPrescriptionOnly !== true)
+//     console.log(notPrescription);
+
+// // 9. Write a function that takes a manufacturer name and returns how many drugs are from that company.
+
+//     function drugsFromManu(manu) {
+//     const drugsCountFromManu = drugs.filter(drug => drug.manufacturer === manu)
+//     console.log(drugsCountFromManu);
+//      const totalStock = drugsCountFromManu.reduce((sum, drugCountFromManu) => sum + drugCountFromManu.stock, 0);
+//         console.log(totalStock);
+//     }
+//     drugsFromManu("Pfizer")
 
 // 10. Use forEach() to count how many drugs are Analgesics.
+
+
 
 
